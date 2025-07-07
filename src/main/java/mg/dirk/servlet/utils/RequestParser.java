754be9365733584950adf3022889d13ea137eeaf
tonymushah <tonyramanantsalama@gmail.com>
2025-07-07 21:45:45 +0300

    `deroulanteParseFunc`

--- src/main/java/mg/dirk/servlet/utils/RequestParser.java
+++ src/main/java/mg/dirk/servlet/utils/RequestParser.java
@@ -3,15 +3,33 @@ package mg.dirk.servlet.utils;
 import java.io.InvalidClassException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.ParseException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import Affichage.Deroulante;
 import mg.dirk.csv.annotations.SkipDeserialization;
 import mg.dirk.reflections.ReflectUtils;
 
 public class RequestParser {
     final HttpServletRequest request;
+
+    public static final String deroulanteParseFunc = "deroulanteParseFunc";
+
+    public static Method getDeroulanteParseFunc(Class<? extends Object> toFind)
+            throws NoSuchMethodException, SecurityException {
+        return toFind.getMethod(deroulanteParseFunc, String.class);
+    }
+
+    public static boolean hasDeroulanteParseFunc(Class<? extends Object> toFind) {
+        try {
+            getDeroulanteParseFunc(toFind);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
     public RequestParser(HttpServletRequest request) {
         this.request = request;
@@ -45,7 +63,12 @@ public class RequestParser {
         }
         for (Field field : ReflectUtils.getFieldsWithExcludedAnnotations(toUseClass, SkipDeserialization.class)) {
             Class<?> fieldType = field.getType();
-            if (fieldType.equals(String[].class)) {
+            if (hasDeroulanteParseFunc(fieldType) && Deroulante.class.isAssignableFrom(fieldType)) {
+                Method parseFunc = getDeroulanteParseFunc(fieldType);
+                Object fieldObj = fieldType.getConstructor().newInstance();
+                parseFunc.invoke(fieldObj, request.getParameter(String.format("%s%s", maybePrefix, field.getName())));
+                ReflectUtils.getFieldSetter(toUseClass, field).invoke(t, fieldObj);
+            } else if (fieldType.equals(String[].class)) {
                 ReflectUtils.getFieldSetter(toUseClass, field).invoke(t,
                         (Object) request.getParameterValues(String.format("%s%s", maybePrefix, field.getName())));
             } else if (ReflectUtils.isSerdable(fieldType)) {
