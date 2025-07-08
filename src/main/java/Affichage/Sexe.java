be5316123a322ba323fb6e67b1c67cedc04886cb
tonymushah <tonyramanantsalama@gmail.com>
2025-07-08 13:22:15 +0300

    added `NOTE`

--- src/main/java/Affichage/Sexe.java
+++ src/main/java/Affichage/Sexe.java
@@ -1,11 +1,16 @@
 package Affichage;
 
-public class Sexe extends Deroulante{
+public class Sexe extends Deroulante {
 
     public Sexe() {
-        String[] cle = {"0","1"};
-        String[] val = {"Vavy","Lahy"};
+        String[] cle = { "0", "1" };
+        String[] val = { "Vavy", "Lahy" };
         super.setCle(cle);
         super.setValeur(val);
+    }
+
+    // NOTE ato no mamadika cle ho lasa deroulante
+    public void deroulanteParseFunc(String paramData) {
+
     }
 }
